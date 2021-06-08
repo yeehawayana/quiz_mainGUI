7489c260d5eff86f0498ee8c7b1a6b51a0e41460
Ayana Lowe-McPherson <>
2021-06-08 22:49:51 +0000

    complete row 1 instructions for users (gave them the instructions to enter a username before they proceed to quiz/results

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import random
 
 class Quiz:
   def __init__(self):
-    #Formatting colours for main GUI (temporary color = light blue)
+    #FORMATTING THE BACKGROUND COLOUR FOR MAIN SCREEN (temporary color = light blue)
     background_color: "light_blue"
 
-    #code for the Quiz frame
+    #CODE FOR MAIN MENU QUIZ GUI
     self.mainquiz_frame = frame(width = 400, height = 400,
                             pady = 10)
     self.mainquiz_frame.grid()
 
-    #Formatting main GUI
+    #FORMATTING MAIN MENU GUI
     self.mainquiz_frame = frame(width = 400, height = 400,
                                 pady = 10, bg = background_color)
     self.main_quiz_frame.grid()
 
-    #Row 0 - QUIZ heading
+    #Row 0 - MAIN MENU QUIZ HEADING
     self.quiz_heading_label = label(self.mainquiz_frame,
                                     text = "QUIZ",
                                     font = "Verdana 20 bold",
@@ -24,4 +24,15 @@ class Quiz:
                                     padx = 10, pady = 10,)
     self.quiz_heading_label.grid(row=0)
 
-    #Row 1 
+    #Row 1 - INSTRUCTIONS ABOUT MAIN MENU GUI FOR USERS  
+    self.quiz_instructions_label = label(text = "Welcome to _ Quiz."
+                                                "Here you will be quized on    your knowledge on _."
+                                                "Please enter a username in order to complete the quiz",
+                                        font = "Helvetic 10 italic", wrap = 300,
+                                        bg = background_color, padx = 10, 
+                                        pady = 10, justify = LEFT)
+    self.quiz_instructions_label.grid(row=1)
+
+    #ROW 2 -  ENTRY FOR GUI
+
+                                                
