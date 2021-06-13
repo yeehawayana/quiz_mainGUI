6790b4ef6cf14bae2f4d89667f48f2ba016b60db
Ayana Lowe-McPherson <81654257+yeehawayana@users.noreply.github.com>
2021-06-13 22:24:50 +0000

    added buttons for quiz and results GUI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import*
+from functools import partial   # To prevent unwanted windows
 import random
 
 class Quiz:
@@ -7,36 +8,50 @@ class Quiz:
     background_color: "light_blue"
 
     #FORMATTING MAIN MENU GUI
-    self.mainmenuquiz_frame = frame(width = 400, height = 400,
-                                pady = 10, bg = background_color)
+    self.mainmenuquiz_frame = frame(width=400, height=400,
+                                pady=10, bg=background_color)
     self.mainmenuquiz_frame.grid()
 
     #Row 0 - MAIN MENU QUIZ HEADING
     self.quiz_heading_label = label(self.mainmenuquiz_frame,
-                                    text = "QUIZ",
-                                    font = "Verdana 20 bold",
-                                    bg = background_color,
-                                    padx = 10, pady = 10,)
+                                    text="QUIZ",
+                                    font="Verdana 20 bold",
+                                    bg=background_color,
+                                    padx=10, pady=10,)
     self.quiz_heading_label.grid(row=0)
 
     #Row 1 - INSTRUCTIONS ABOUT MAIN MENU GUI FOR USERS  
-    self.quiz_instructions_label = label(text = "Welcome to _ Quiz."
-                                                "Here you will be quized on    your knowledge on _."
-                                                "Please enter a username in order to complete the quiz",
-                                        font = "Helvetic 10 italic", wrap = 300,
-                                        bg = background_color, padx = 10, 
-                                        pady = 10, justify = LEFT)
+    self.quiz_instructions_label = label(text="Welcome to _ Quiz."
+                                              "Here you will be quized on    your knowledge on _."
+                                              "Please enter a username in order to complete the quiz",
+                                        font="Helvetic 10 italic", wrap=300,
+                                        bg=background_color, padx=10, 
+                                        pady=10, justify=LEFT)
     self.quiz_instructions_label.grid(row=1)
 
     #ROW 2 -  ENTRY FOR GUI
     self.username_entry = Entry(self.mainmenuquiz_frame,
-                                width = 20, font = "Arial 14 bold")
+                                width=20, font ="Arial 14 bold")
     self.username_entry.grid(row=2)
     
-    #ROW 3 - BUTTONS 
+    #ROW 3 - WARNING MESSAGE
+    
+    #ROW 4 - QUIZ AND RESULT BUTTONS
+    self.quiz_results_frame = frame(self.mainmenuquiz_frame)
+    self.quiz_results_frame.grid(row=4)
+    
+    #ROW 4 - QUIZ BUTTON 
+    self.quiz_button = button(self.quiz_results_frame, width=10,
+                              text="Quiz yourself")
+    self.quiz_button.grid(row=0, column=0)
+
+    #ROW 4 - RESULTS BUTTON
+    self.results_button = button(self.quiz_results_frame, width=10,
+                                text="Results")
+    self.results_button.grid(row=0, column=1)
 
     
-#Main Routine
+#Main Routine #Problem with 1 positional argument
 if __name__ == "__main__":
   root = Tk()
   root.title("Main Menu")
